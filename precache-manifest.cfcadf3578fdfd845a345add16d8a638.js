371244a8524838bdd14714f3db87fd0add0beec0
Emad <almsouti.e@gmail.com>
2021-04-17 07:32:54 +0200

    Updates

---
+++ precache-manifest.cfcadf3578fdfd845a345add16d8a638.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b69c7eac441d6c5d44a1c1b7e869c0e",
+    "url": "/sidebar/index.html"
+  },
+  {
+    "revision": "fd1fd1a38b3eb6d95bbc",
+    "url": "/sidebar/static/css/main.e0a521cd.chunk.css"
+  },
+  {
+    "revision": "84e44487579eef8a3998",
+    "url": "/sidebar/static/js/2.6bcace1a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/sidebar/static/js/2.6bcace1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1fd1a38b3eb6d95bbc",
+    "url": "/sidebar/static/js/main.9d1f374b.chunk.js"
+  },
+  {
+    "revision": "05b276f4552a05861b70",
+    "url": "/sidebar/static/js/runtime-main.e6e11059.js"
+  },
+  {
+    "revision": "19579303fe74188e554c93d5adeb5f35",
+    "url": "/sidebar/static/media/logo.19579303.svg"
+  }
+]);
